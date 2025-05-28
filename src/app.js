f2cc952b9f0619411426ff39220e8f331178f3b3
daniel3034 <danielh3034@gmail.com>
2025-05-28 16:50:43 -0500

    install swagger formatting

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import routes from './routes/index.js';
 import client from './models/db.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const app = express();
 app.use(express.json());
+
+// Swagger setup
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'CSE341 Project 2 API',
+      version: '1.0.0',
+      description: 'API documentation for CSE341 Project 2'
+    }
+  },
+  apis: ['./src/routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/api', routes);
 
 const PORT = 8080;
